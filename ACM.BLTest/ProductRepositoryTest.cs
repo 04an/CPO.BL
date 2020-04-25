af5e267fb1f7367bb137168a64267d5b9c86a896
04an <olegdzhum@gmail.com>
2020-04-25 17:23:46 +0200

    Add unit test for Productrepository.Retrieve()

---
+++ ACM.BLTest/ProductRepositoryTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ACM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class ProductRepositoryTest
+    {
+        [TestMethod]
+        public void RetrieveValid()
+        {
+            //--Arrange
+            var ProdRepository = new ProductRepository();
+            var expected = new Product(2)
+            {
+                ProductName = "Water",
+                Description = "Sparkling water",
+                CurrentPrice = 2.20M
+            };
+
+            //--Act
+            var actual = ProdRepository.Retrieve(2);
+
+            //--Assert
+            Assert.AreEqual(expected.ProductId, actual.ProductId);
+            Assert.AreEqual(expected.ProductName, actual.ProductName);
+            Assert.AreEqual(expected.Description, actual.Description);
+            Assert.AreEqual(expected.CurrentPrice, actual.CurrentPrice);
+        }
+    }
+}
