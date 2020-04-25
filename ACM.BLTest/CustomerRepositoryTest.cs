dc4bcf6870ff1f51e4335c1f858310d2a9895d93
04an <olegdzhum@gmail.com>
2020-04-25 17:10:07 +0200

    Add unit test for CustomerRepository.Retrieve()

---
+++ ACM.BLTest/CustomerRepositoryTest.cs
@@ -0,0 +1,33 @@
+﻿using ACM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerRepositoryTest
+    {
+        [TestMethod]
+        public void RetrieveValid()
+        {
+            //--Arrange
+            var custRepository = new CustomerRepository();
+            var expected = new Customer(1)
+            {
+                EmailAddress = "monkey@gmail.com",
+                FirstName = "Paul",
+                LastName = "Laspapos"
+            };
+
+            //--Act
+            var actual = custRepository.Retrieve(1);
+
+            //--Assert
+            Assert.AreEqual(expected.CustId, actual.CustId);
+            Assert.AreEqual(expected.EmailAddress, actual.EmailAddress);
+            Assert.AreEqual(expected.FirstName, actual.FirstName);
+            Assert.AreEqual(expected.LastName, actual.LastName);
+        }
+       
+    }
+}
