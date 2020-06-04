76002c51051439cf46b4e9f420eb3b84c4b52f94
04an <olegdzhum@gmail.com>
2020-06-04 13:51:45 +0200

    Add Unit test for LogginService WriteToFile() method.

---
+++ Test/Acm.CommonTest/LogginServiceTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Acm.Common;
+using ACM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Acm.CommonTest
+{
+    [TestClass]
+    public class LogginServiceTest
+    {
+        [TestMethod]
+        public void WriteToFileTest()
+        {
+            //Arrange
+            var changeItems = new List<ILoggable>();
+
+            var customer = new Customer(1)
+            {
+                EmailAddress = "monkey@gmail.com",
+                FirstName = "Paul",
+                LastName = "Laspapos",
+                AddressList = null
+            };
+            changeItems.Add(customer);
+
+            var product = new Product(2)
+            {
+                ProductName = "Water",
+                Description = "Sparkling water",
+                CurrentPrice = 2.20M
+            };
+            changeItems.Add(product);
+
+            //Act
+            LogginService.WriteToFile(changeItems);
+
+            //Asset
+            //nothing
+        }
+    }
+}
